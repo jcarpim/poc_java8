864a759bf49e44b48e2febf15cfe958a08014d3a
jcarpim <jecarpim@gmail.com>
2019-12-19 20:18:45 -0300

    criação da classe main2

---
+++ poc_jess_java8/src/poc_jess_java8/Main2.java
@@ -0,0 +1,16 @@
+package poc_jess_java8;
+
+interface Numero{
+	Double getNum();
+}
+public class Main2 {
+
+	public static void main(String[] args) {
+		Numero  num ;
+		num = () -> 6.7;
+		System.out.println(num.getNum());
+		
+		
+	}
+
+}
